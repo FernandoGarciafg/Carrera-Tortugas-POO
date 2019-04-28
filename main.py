623ef7252fad4e0e6287aeccd14e7c59200129e1
Fernando García <fernandogarcia_fg@outlook.com>
2019-04-28 15:54:32 -0300

    Creacion del circuito y los corredores con turtle y POO

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito(): # creamos un objeto que va a ser el circuito
     corredores = [] # la clase circuito va a tener como atributos los corredores y la pantalla
@@ -28,12 +29,23 @@ class Circuito(): # creamos un objeto que va a ser el circuito
             
             self.corredores.append(new_turtle)
             
-    
-
-
+    def competir(self):
+        hayGanador = False
+        
+        while not hayGanador:
+            for tortuga in self.corredores:
+                
+                avance = random.randint(1, 6)
+                tortuga.forward(avance)
+                
+                if tortuga.position()[0] >= self.__finishLine:
+                    hayGanador = True
+                    print("La tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                    break
+                
 if __name__ == '__main__':
     circuito = Circuito(640, 480)
-        
+    circuito.competir()
         
 # c = Circuito(320, 320) crear instancia del objeto                                       
                                        
