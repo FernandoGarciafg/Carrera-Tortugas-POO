4385309bf58f129a61acfd5bd71fb517ec41a848
Fernando García <fernandogarcia_fg@outlook.com>
2019-04-28 14:44:17 -0300

    Creacion del circuito y los corredores con turtle y POO

---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle
+
+class Circuito(): # creamos un objeto que va a ser el circuito
+    corredores = [] # la clase circuito va a tener como atributos los corredores y la pantalla
+    __posStartY = (-30, -10, 10 , 30)
+    __colorTurtle = ('red', 'blue', 'green', 'orange')
+    
+    def __init__(self, width, height): # creamos el constructor de nuestra clase con los atributos ancho y altura que van a ser los
+                                       # de la pantalla de nuestro juego
+        self.width = width
+        self. height = height
+        
+        self.__screen = turtle.Screen()
+        self.__screen.setup(width, height)
+        self.__screen.bgcolor('lightgray')
+        self.__startLine = -width / 2 + 20
+        self.__finishLine = width / 2 - 20
+        
+        self.__crearCorredores()
+        
+    def __crearCorredores(self):
+        for i in range(4):
+            new_turtle = turtle.Turtle()
+            new_turtle.color(self.__colorTurtle[i])
+            new_turtle.shape('turtle')
+            new_turtle.penup()
+            new_turtle.setpos(self.__startLine, self.__posStartY[i])
+            
+            self.corredores.append(new_turtle)
+
+
+
+
+if __name__ == '__main__':
+    circuito = Circuito(640, 480)
+        
+        
+# c = Circuito(320, 320) crear instancia del objeto                                       
+                                       
